95cddb2a6b6b6686ac2666422c0fe5807503d63f
Marina Igumnova <igum.marina@gmail.com>
2017-02-14 19:38:22 +0300

    WA-5:WIP Update table style to wrap all the columns

--- src/components/WagonRow.js
+++ src/components/WagonRow.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {TableRow, TableRowColumn} from 'material-ui/Table';
 import "./WagonItem"
 
+export const overallStyle = {
+    borderRight: "1px solid black",
+    fontSize: "11px",
+    textAlign: "center",
+    width: "auto",
+    padding: "0",
+    margin: "0",
+    wordWrap: 'break-word',
+    whiteSpace: 'normal'
+};
+let oldStyle = { backgroundColor: '#f9f768'};
+let newStyle = { backgroundColor: '#b3d84b'};
+export const width = (width) => ({width});
 const WagonRow = ({
     id,
     number,
@@ -26,33 +39,35 @@ const WagonRow = ({
     wagonType,
     wagonModel,
     km,
-    comment
-}) => (
-    <TableRow>
-        <TableRowColumn>{number}</TableRowColumn>
-        <TableRowColumn>{wagon}</TableRowColumn>
-        <TableRowColumn>{owner}</TableRowColumn>
-        <TableRowColumn>{manager}</TableRowColumn>
-        <TableRowColumn>{releaseDate}</TableRowColumn>
-        <TableRowColumn>{releaseStation}</TableRowColumn>
-        <TableRowColumn>{releaseRailroad}</TableRowColumn>
-        <TableRowColumn>{cargoType}</TableRowColumn>
-        <TableRowColumn>{weight}</TableRowColumn>
-        <TableRowColumn>{operationDate}</TableRowColumn>
-        <TableRowColumn>{currentStation}</TableRowColumn>
-        <TableRowColumn>{currentRailroad}</TableRowColumn>
-        <TableRowColumn>{operationCode}</TableRowColumn>
-        <TableRowColumn>{arrivalDate}</TableRowColumn>
-        <TableRowColumn>{arrivalStation}</TableRowColumn>
-        <TableRowColumn>{arrivalRailroad}</TableRowColumn>
-        <TableRowColumn>{cargoCode}</TableRowColumn>
-        <TableRowColumn>{repairDate}</TableRowColumn>
-        <TableRowColumn>{idleDays}</TableRowColumn>
-        <TableRowColumn>{wagonType}</TableRowColumn>
-        <TableRowColumn>{wagonModel}</TableRowColumn>
-        <TableRowColumn>{km}</TableRowColumn>
-        <TableRowColumn>{comment}</TableRowColumn>
+    comment,
+    isOld
+}) => {
+    let style ={...overallStyle, ...(isOld ? oldStyle : newStyle)};
+    return <TableRow>
+        <TableRowColumn style={{...style, ...width('2%')}}>{number}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('5%')}}>{wagon}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('7%')}}>{owner}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('7%')}}>{manager}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('5%')}}>{releaseDate}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('10%')}}>{releaseStation}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('6%')}}>{releaseRailroad}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('5%')}}>{cargoType}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('4%')}}>{weight}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('5%')}}>{operationDate}</TableRowColumn>
+        <TableRowColumn style={{...style, ...width('7%')}}>{currentStation}</TableRowColumn>
+        <TableRowColumn style={style}>{currentRailroad}</TableRowColumn>
+        <TableRowColumn style={style}>{operationCode}</TableRowColumn>
+        <TableRowColumn style={style}>{arrivalDate}</TableRowColumn>
+        <TableRowColumn style={style}>{arrivalStation}</TableRowColumn>
+        <TableRowColumn style={style}>{arrivalRailroad}</TableRowColumn>
+        <TableRowColumn style={style}>{cargoCode}</TableRowColumn>
+        <TableRowColumn style={style}>{repairDate}</TableRowColumn>
+        <TableRowColumn style={style}>{idleDays}</TableRowColumn>
+        <TableRowColumn style={style}>{wagonType}</TableRowColumn>
+        <TableRowColumn style={style}>{wagonModel}</TableRowColumn>
+        <TableRowColumn style={style}>{km}</TableRowColumn>
+        <TableRowColumn style={style}>{comment}</TableRowColumn>
     </TableRow>
-);
+};
 
 export default WagonRow;
--- src/components/WagonTablePresentation.js
+++ src/components/WagonTablePresentation.js
@@ -1,49 +1,47 @@
 import React from 'react';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow} from 'material-ui/Table';
 import WagonRow from './WagonRow';
+import {width} from "./WagonRow";
+import {overallStyle} from "./WagonRow";
 
 const WagonTablePresentation = ({wagons}) => {
     console.log(JSON.stringify(wagons));
     return (
-        <div>
+        <div style={{width: "auto"}}>
             <Table showCheckboxes={false}>
-                <TableHeader>
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
                     <TableRow>
-                        <TableHeaderColumn>N</TableHeaderColumn>
-                        <TableHeaderColumn>Вагон</TableHeaderColumn>
-                        <TableHeaderColumn>Собственник</TableHeaderColumn>
-                        <TableHeaderColumn>Распорядитель</TableHeaderColumn>
-                        <TableHeaderColumn>Дата отправки</TableHeaderColumn>
-                        <TableHeaderColumn>Станция отправления</TableHeaderColumn>
-                        <TableHeaderColumn>ЖД Отправления</TableHeaderColumn>
-                        <TableHeaderColumn>Тип груза</TableHeaderColumn>
-                        <TableHeaderColumn>Вес</TableHeaderColumn>
-                        <TableHeaderColumn>Дата операции</TableHeaderColumn>
-                        <TableHeaderColumn>Текущая станция</TableHeaderColumn>
-                        <TableHeaderColumn>ЖД текущая</TableHeaderColumn>
-                        <TableHeaderColumn>Код операции</TableHeaderColumn>
-                        <TableHeaderColumn>Дата прибытия</TableHeaderColumn>
-                        <TableHeaderColumn>Станция назначения</TableHeaderColumn>
-                        <TableHeaderColumn>ЖД назначения</TableHeaderColumn>
-                        <TableHeaderColumn>Код операции</TableHeaderColumn>
-                        <TableHeaderColumn>Дата прибытия</TableHeaderColumn>
-                        <TableHeaderColumn>Станция назначения</TableHeaderColumn>
-                        <TableHeaderColumn>ЖД назначения</TableHeaderColumn>
-                        <TableHeaderColumn>Код груза</TableHeaderColumn>
-                        <TableHeaderColumn>Дата планового ремонта</TableHeaderColumn>
-                        <TableHeaderColumn>Количество дней простоя</TableHeaderColumn>
-                        <TableHeaderColumn>Тип вагона</TableHeaderColumn>
-                        <TableHeaderColumn>Модель</TableHeaderColumn>
-                        <TableHeaderColumn>Км</TableHeaderColumn>
-                        <TableHeaderColumn>Комментарий</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('2%')}}>N</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('5%')}}>Вагон</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('7%')}}>Собственник</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('7%')}}>Распорядитель</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('5%')}}>Дата отправки</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('10%')}}>Станция отправления</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('6%')}}>ЖД Отправления</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('5%')}}>Тип груза</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('4%')}}>Вес</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('5%')}}>Дата операции</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle, ...width('7%')}}>Текущая станция</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>ЖД текущая</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Код операции</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Дата прибытия</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Станция назначения</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>ЖД назначения</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Код груза</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Дата планового ремонта</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Количество дней простоя</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Тип вагона</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Модель</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Км</TableHeaderColumn>
+                        <TableHeaderColumn style={{...overallStyle}}>Комментарий</TableHeaderColumn>
                     </TableRow>
                 </TableHeader>
             </Table>
             {Object.keys(wagons).sort().map((number) =>
                 <Table key={number} showCheckboxes={false}>
                     <TableBody >
-                        <WagonRow {...wagons[number].old} />
-                        {wagons[number].updated ? <WagonRow {...wagons[number].updated} /> : null}
+                        <WagonRow isOld={true} {...wagons[number].old} />
+                        {wagons[number].updated ? <WagonRow isOld={false} {...wagons[number].updated} /> : null}
                     </TableBody>
                 </Table>
             )

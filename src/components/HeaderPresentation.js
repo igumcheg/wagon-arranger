e7a31e0d981856dcedd2f4f5f7650470185f4464
Marina Igumnova <igum.marina@gmail.com>
2017-03-07 19:01:19 +0300

    New header added

--- src/components/HeaderPresentation.js
+++ src/components/HeaderPresentation.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import AddDislocationForm from "./AddDislocationForm";
+import {Component} from 'react'
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FileFileUpload from 'material-ui/svg-icons/file/file-upload';
+import {loadDislocationFromFile} from "../util/util";
+import {newWagonsAdded} from "../actions/actions";
+import {connect} from 'react-redux';
 
-let onClick = () => {
-    console.log("clicked");
-};
+class HeaderPresentation extends Component {
+    render() {
+        return (
+            <AppBar title="Раума Карго" iconElementLeft={
+               <div> <input id="myInput" type="file" ref={(ref) => this.myInput = ref} style={{ display: 'none' }}
+               onChange= {this.props.onChange}/>
+                 <IconButton>
+                    <FileFileUpload color={'white'}
+                     onClick={(e) => this.myInput.click()} />
 
-const HeaderPresentation = ({
-    mainLoaded,
-    addLoaded,
-    mainFilename,
-    addFilename,
-}) => (
-    <div>
-        {mainLoaded ?
-            <div>
-                Загружен основной файл: {mainFilename}
-                <AddDislocationForm />
-            </div>
-            : null}
-        {addLoaded ?
-            <div>
-                Загружен дополнительный файл: {addFilename}
-            </div>
-            : null}
-    </div>
-);
+                 </IconButton>
+                 </div>}
+            />
+        );
+    }
 
-export default HeaderPresentation;
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    onChange: (event) => {
+        let file = event.target.files[0];
+        loadDislocationFromFile(file, dispatch, newWagonsAdded);
+    }
+});
+
+export default HeaderPresentation = connect(null, mapDispatchToProps)(HeaderPresentation);
 
